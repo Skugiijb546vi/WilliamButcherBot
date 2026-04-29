64e245e04c1ddc232c6fd1c7875dc403173f89ff
Skugiijb546vi <67493589+Skugiijb546vi@users.noreply.github.com>
2026-04-29 06:53:20 +0300

    Update dice.py

--- wbb/modules/dice.py
+++ wbb/modules/dice.py
@@ -3,10 +3,11 @@ from pyrogram.types import Message
 
 from wbb import SUDOERS, USERBOT_PREFIX, app, app2
 
-__MODULE__ = "Dice"
+__MODULE__ = "زار"
 __HELP__ = """
 /dice
-    Roll a dice.
+    هاویشتنی زارێک.
+    (ئەگەر ئەدمینی باڵا بیت، هەمیشە ژمارە ٦ت بۆ دەردەچێت!)
 """
 
 
@@ -18,12 +19,15 @@ __HELP__ = """
 )
 @app.on_message(filters.command("dice"))
 async def throw_dice(client, message: Message):
+    # پشکنین بۆ ئەوەی بزانین نێرەرەکە ئەدمینی باڵایە یان نا
     six = (message.from_user.id in SUDOERS) if message.from_user else False
 
     c = message.chat.id
     if not six:
+        # بۆ بەکارهێنەری ئاسایی، تەنها یەک زار بهاوێژە
         return await client.send_dice(c, "🎲")
 
+    # بۆ ئەدمینی باڵا، ئەوەندە بهاوێژە تا ٦ دەردەچێت
     m = await client.send_dice(c, "🎲")
 
     while m.dice.value != 6:
