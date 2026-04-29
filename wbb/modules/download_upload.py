9b5b4e6f2a7b621e8096354c3d07ddc565264602
Skugiijb546vi <67493589+Skugiijb546vi@users.noreply.github.com>
2026-04-29 06:54:49 +0300

    Update download_upload.py

--- wbb/modules/download_upload.py
+++ wbb/modules/download_upload.py
@@ -1,25 +1,6 @@
 """
 MIT License
-
 Copyright (c) 2024 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 """
 
 from os import remove
@@ -47,11 +28,11 @@ async def download_func(_, message: Message):
     start = time()
 
     body = {
-        "Started": ctime(start),
+        "دەستپێکردن": ctime(start),
     }
     m = await eor(
         message,
-        text=section("Downloading", body),
+        text=section("خەریکی داونلۆدکردنم", body),
     )
 
     if reply:
@@ -60,10 +41,10 @@ async def download_func(_, message: Message):
             task_name="Downloader",
         )
 
-        body["Task ID"] = task_id
+        body["ئایدی ئەرک (Task ID)"] = task_id
         await eor(
             m,
-            text=section("Downloading", body),
+            text=section("خەریکی داونلۆدکردنم", body),
         )
 
         await task
@@ -72,24 +53,24 @@ async def download_func(_, message: Message):
         file = task.result()
 
         elapsed = int(time() - start)
-        body["File"] = file.split("/")[-1]
-        body["Took"] = f"{elapsed}s"
+        body["فایل"]: file.split("/")[-1]
+        body["ماوەی خایەنراو"]: f"{elapsed} چرکە"
 
-        return await eor(m, text=section("Downloaded", body))
+        return await eor(m, text=section("داونلۆد کرا ✅", body))
 
     text = message.text
     if len(text.split()) < 2:
-        return await eor(m, text="Invalid Arguments")
+        return await eor(m, text="زانیارییەکان هەڵەن (لینکت نەداوە)")
 
     url = text.split(None, 1)[1]
 
     try:
         task, task_id = await download(url)
 
-        body["Task ID"] = task_id
+        body["ئایدی ئەرک (Task ID)"] = task_id
         await eor(
             m,
-            text=section("Downloading", body),
+            text=section("خەریکی داونلۆدکردنم", body),
         )
 
         await task
@@ -99,13 +80,13 @@ async def download_func(_, message: Message):
     except Exception as e:
         e = format_exc()
         e = e.splitlines()[-1]
-        return await eor(m, text=f"**Error:** `{str(e)}`")
+        return await eor(m, text=f"**هەڵە:** `{str(e)}`")
 
     elapsed = int(time() - start)
-    body["File"] = file.split("/")[-1]
-    body["Took"] = f"{elapsed}s"
+    body["فایل"] = file.split("/")[-1]
+    body["ماوەی خایەنراو"] = f"{elapsed} چرکە"
 
-    await eor(m, text=section("Downloaded", body))
+    await eor(m, text=section("داونلۆد کرا ✅", body))
 
 
 @app2.on_message(
@@ -116,17 +97,17 @@ async def download_func(_, message: Message):
 )
 async def upload_func(_, message: Message):
     if len(message.text.split()) != 2:
-        return await eor(message, text="Invalid Arguments")
+        return await eor(message, text="زانیارییەکان هەڵەن (ناونیشانی فایلت نەداوە)")
 
     url_or_path = message.text.split(None, 1)[1]
 
     start = time()
 
     body = {
-        "Started": ctime(start),
+        "دەستپێکردن": ctime(start),
     }
 
-    m = await eor(message, text=section("Uploading", body))
+    m = await eor(message, text=section("خەریکی ئەپڵۆدکردنم", body))
 
     async def upload_file(path: str):
         task, task_id = await add_task(
@@ -135,19 +116,19 @@ async def upload_func(_, message: Message):
             path,
         )
 
-        body["Task ID"] = task_id
+        body["ئایدی ئەرک (Task ID)"] = task_id
         await eor(
             m,
-            text=section("Uploading", body),
+            text=section("خەریکی ئەپڵۆدکردنم", body),
         )
 
         await task
         await rm_task(task_id)
 
         elapsed = int(time() - start)
-        body["Took"] = f"{elapsed}s"
+        body["ماوەی خایەنراو"] = f"{elapsed} چرکە"
 
-        return await eor(m, text=section("Uploaded", body))
+        return await eor(m, text=section("ئەپڵۆد کرا ✅", body))
 
     try:
         if isfile(url_or_path):
@@ -155,10 +136,10 @@ async def upload_func(_, message: Message):
 
         task, task_id = await download(url_or_path)
 
-        body["Task ID"] = task_id
+        body["ئایدی ئەرک (Task ID)"] = task_id
         await eor(
             m,
-            text=section("Downloading", body),
+            text=section("خەریکی داونلۆدکردنی کاتیی فایلم", body),
         )
         await task
         file = task.result()
@@ -169,4 +150,4 @@ async def upload_func(_, message: Message):
     except Exception as e:
         e = format_exc()
         e = e.splitlines()[-1]
-        return await eor(m, text=f"**Error:** `{str(e)}`")
+        return await eor(m, text=f"**هەڵە:** `{str(e)}`")
